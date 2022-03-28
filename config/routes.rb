ada8c3f48840cfe5123f1ade62a3c97b3000ad0e
Sami Ullah Khan <samiullahkhan@pk.folio3.com>
2022-03-28 20:32:36 +0500

    Crud Updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'surprise-for-you', to: 'pages#surprise'
   resources :datums, only: [:index, :new, :show, :create, :edit, :update, :destroy ]
 end
